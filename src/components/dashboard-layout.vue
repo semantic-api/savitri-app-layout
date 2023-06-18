e7b7870f78731149060e1155b98c1d05893a7c95
ringeringeraja <joaosan177@gmail.com>
2023-06-18 16:51:44 -0300

    fix: topbar bug

--- src/components/dashboard-layout.vue
+++ src/components/dashboard-layout.vue
@@ -1,38 +1,9 @@
 <script setup lang="ts">
-import { inject, ref, watch } from 'vue'
+import { inject } from 'vue'
 import { SvIcon, SvPicture } from '@savitri/ui'
 import MainNavbar from '../components/main-navbar.vue'
 
 const menuSchema = inject('menuSchema')
-const topbarVisible = ref(false)
-const topbarComponent = ref(null)
-
-let topbarObserver: MutationObserver
-watch(topbarComponent, () => {
-  topbarVisible.value = false
-  topbarObserver?.disconnect()
-
-  const topbarElem = document.getElementById('inner-topbar')
-  if( !topbarElem ) {
-    return
-  }
-
-  if( topbarElem.children.length > 0 ) {
-    topbarVisible.value = true
-  }
-
-  topbarObserver = new MutationObserver(() => {
-    if( topbarElem.children.length > 0 ) {
-      topbarVisible.value = true
-      topbarObserver.disconnect()
-    }
-  })
-
-  topbarObserver.observe(topbarElem, {
-    childList: true
-  })
-
-}, { immediate: true })
 </script>
 
 <template>
@@ -49,17 +20,7 @@ watch(topbarComponent, () => {
             {{ viewTitle }}
           </sv-icon>
 
-          <div
-            v-if="$route.matched.slice(-1)[0].components.topbar"
-            id="inner-topbar"
-            ref="topbarComponent"
-            :style="{
-              display: topbarVisible
-                ? 'inherit'
-                : 'none'
-            }"
-            class="dashboard__topbar"
-          >
+          <div class="dashboard__topbar">
             <router-view name="topbar"></router-view>
           </div>
 
